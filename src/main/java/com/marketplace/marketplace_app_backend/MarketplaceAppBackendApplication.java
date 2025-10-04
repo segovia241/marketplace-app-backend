ccb522bee6cb8af6bd285e8c5cabf769941b5688
segovia241 <segovia.network@gmail.com>
2025-10-04 04:06:29 -0500

    main

---
+++ src/main/java/com/marketplace/marketplace_app_backend/MarketplaceAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.marketplace.marketplace_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplaceAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketplaceAppBackendApplication.class, args);
+	}
+
+}
